6759f0df7261a279c895eea8518414ca025e1654
Digital Creations <dc-email@example.com>
2026-02-06 10:38:57 +0530

    Chat Socket

--- src/ui/components/chat/socket.ts
+++ src/ui/components/chat/socket.ts
@@ -1,13 +1,8 @@
 import { io } from "socket.io-client";
 
-// export const socket = io("http://localhost:5000", {
-//   auth: {
-//     token: localStorage.getItem("token")
-//   },
-//   transports: ["websocket"]
-// });
-
 export const socket = io("http://localhost:5000", {
-  autoConnect: false,
-  transports: ["websocket"],
+  auth: {
+    token: localStorage.getItem("token")
+  },
+  transports: ["websocket"]
 });
